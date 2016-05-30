cb255c648a40a1d89a8a7bdb6f5874aa33e99916
carmenlm <carmenlopezmedina@yahoo.es>
2016-05-30 23:08:32 +0200

    upload

--- js/script.js
+++ js/script.js
@@ -8,19 +8,27 @@ $(document).ready(function () {
 
         console.log('arranco');
 
+        //muestro la tabla
         $("#tabla").show();
 
+        //peticion ajax jquery
         $.ajax({
+            //url con los datos json
             url: "js/customers.json",
+            //funcion si hay error
             error: function () {
                 $('#info').html('<p>An error has occurred</p>');
             },
+            //tipo de datos
             dataType: 'json',
+            //funcion si hay exit
             success: function (data, response) {
                 console.log('exito');
+                //llamo al callback
                 myCallBackDeCustomers(data);
 
             },
+            //tipo de peticion
             type: 'GET'
         });
     });
